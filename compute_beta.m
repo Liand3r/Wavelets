53b0ef7b00564a7c044315811a0cbc02d5d6d261
Charlotte Girardot <charlotte.girardot@ensimag.grenoble-inp.fr>
2017-01-31 00:36:11 +0100

    La merde

--- compute_beta.m
+++ compute_beta.m
@@ -1,28 +1,21 @@
-function [ Beta] = compute_beta( X )
+function [ Beta] = compute_beta( Y, sig_est_Y2, sig_est )
 
-n = size(X,1);
-
-%Calul de sig_est_X2
-sig_est_X2 = 0;
-for i = 1:n
-    for j = 1:n
-        sig_est_X2 = sig_est_X2 + X(i,j)^2;
-    end
-end
-sig_est_X2 = sig_est_X2 / (n^2);
+n = size(Y,1);
 
 %calcul de kappa
-kappa_est_X = 0;
+kappa_est_Y = 0;
 for i = 1:n
     for j = 1:n
-        kappa_est_X = kappa_est_X + X(i,j)^4;
+        kappa_est_Y = kappa_est_Y + Y(i,j)^4;
     end
 end
-kappa_est_X = kappa_est_X / (sig_est_X2^2 * n^2);
+kappa_est_Y = kappa_est_Y / (sig_est_Y2^2 * n^2);
 
 %Estimation de beta
 syms cbeta
-eqn = kappa_est_X == gamma(1/cbeta) * gamma(5/cbeta) / (((gamma(3/cbeta))^2));
+%d'après l'equation 28 du papier
+% j'avoue que je ne saurais pas le redémontrer
+eqn = kappa_est_Y ==  (1/(sig_est_Y2^2)) * ( 6 * sig_est^2 * sig_est_Y2 - 3 * sig_est^4 + (sig_estY2 - sig_est^2)^2 * (gamma(1/cbeta) * gamma(5/cbeta) / ((gamma(3/cbeta))^2)) );
 solx = solve(eqn, cbeta);
 
 Beta = solx;
