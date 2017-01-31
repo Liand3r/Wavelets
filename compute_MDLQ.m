28f6f18e67944d1b0fd7d56eae27ea73521924dc
Charlotte Girardot <charlotte.girardot@ensimag.grenoble-inp.fr>
2017-01-31 02:21:51 +0100

    Bis bis bis bis BITE BITE BITE BITE

--- compute_MDLQ.m
+++ compute_MDLQ.m
@@ -1,7 +1,8 @@
-function [ MDLQ ] = compute_MDLQ( Y, m, delta, sigma_est)
-
+function [ MDLQ ] = compute_MDLQ( Y, m, delta, sigma_est, T)
+%Voir équation 31
+% il s'agit du truc à minimiser
 MDLQ = 0;
-Xq_est = compute_Xq_est(Y, m, delta);
+Xq_est = compute_Xq_est(Y, m, delta, T);
 for i = 1:n
     for j = 1:n
         MDLQ = (Y(i,j) - Xq_est(i,j))^2;
